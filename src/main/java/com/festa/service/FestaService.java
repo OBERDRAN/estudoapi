7c704a05d595f622a3532269fbe87f2b6687baf9
oberdran123@gmail.com <oberdran123@gmail.com>
2024-08-12 12:03:03 -0300

    alteração no metodo findbyid

--- src/main/java/com/festa/service/FestaService.java
+++ src/main/java/com/festa/service/FestaService.java
@@ -1,6 +1,5 @@
 package com.festa.service;
 
-import ch.qos.logback.classic.joran.sanity.IfNestedWithinSecondPhaseElementSC;
 import com.festa.model.FestaModel;
 import com.festa.repository.FestaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,22 +24,28 @@ public class FestaService {
 
     //listar por id
     public Optional<FestaModel>listarid(Long id){
-        return fp.findById(id);
-    }
 
+        if(fp.existsById(fm.getId())){
+            fp.findById(id);
+        }else{
+            System.out.println("Nenhuma festa com esse ID");
+        }
+        //tentando entender ainda esse codigo abaixo, mas serve pra caso
+        // não encontre nenhum registro
+        return Optional.empty();
+    }
     //salvar festa
-    public void festasalvar(FestaModel festamodel){
-        fp.save(festamodel);
-    }
+     public void festasalvar(FestaModel festamodel){
+            fp.save(festamodel);
+     }
 
-    //alterar
-    public void festaalterar(FestaModel festaModel){
-        if (fp.existsById(festaModel.getId())){
+     //alterar
+     public void festaalterar(FestaModel festaModel){
+        if(fp.existsById(festaModel.getId())){
             fp.save(festaModel);
         }else{
             System.out.println("nenhuma festa encontrada");
         }
-    }
-    //implementar busca por nome
+     }
 
 }
