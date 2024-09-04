9771e0194e769a7301a826b200ca159ebcc725b6
oberdran123@gmail.com <oberdran123@gmail.com>
2024-09-04 11:29:04 -0300

    colocado status da resposta ao acessar métodos

--- src/main/java/com/festa/controller/FestaController.java
+++ src/main/java/com/festa/controller/FestaController.java
@@ -3,6 +3,7 @@ package com.festa.controller;
 import com.festa.model.FestaModel;
 import com.festa.service.FestaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +14,14 @@ import java.util.Optional;
 public class FestaController {
     @Autowired
     private FestaService service;
-    @GetMapping("/listarfestas")
     //listar todos
+    @GetMapping("/listarfestas")
+    @ResponseStatus(code = HttpStatus.OK)
     public List<FestaModel> listarFestaTodos(){
         return service.listarFesta();
     }
     @GetMapping("/id")
+    @ResponseStatus(code = HttpStatus.OK)
     public Optional<FestaModel> listarFestaId(long id){
         return service.listarFestaId(id);
     }
@@ -31,14 +34,17 @@ public class FestaController {
 //        return ResponseEntity.ok().build();
 //    }
     @PostMapping("/salvar")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public FestaModel salvarFesta1(FestaModel festamodel){
         return service.salvarFesta1(festamodel);
     }
     @PostMapping("/alterar")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void alterarFesta(FestaModel festamodel){
         service.festaalterar(festamodel);
     }
     @GetMapping("/hello")
+    @ResponseStatus(code=HttpStatus.OK)
     public String Hello(){
         return service.Hello();
     }
